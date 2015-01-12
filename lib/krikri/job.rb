918a59eeb39d1f4994ca3303453e3a5a04edce1d
Tom Johnson <thomas.johnson@oregonstate.edu>
2015-01-11 17:36:45 -0800

    Refactor Jobs extracting general behavior

--- lib/krikri/job.rb
+++ lib/krikri/job.rb
@@ -5,9 +5,22 @@ module Krikri
   class Job
     @queue = nil
     ##
-    # @abstract Perform the job; called by the queue system
-    def self.perform(*)
-      fail NotImplementedError
+    # Perform the job.
+    def self.perform(activity_id)
+      activity = Krikri::Activity.find(activity_id)
+      opts = JSON.parse(activity['opts'], symbolize_names: true)
+      agent = activity['agent'].constantize.new(opts)
+      activity.run { run(agent) }
+    end
+
+    private
+
+    ##
+    # @abstract run the job's task. Implement the actual task
+    #   against the agent passed in.
+    # @param agent  the agent to run the task
+    def self.run(_)
+      raise NotImplementedError
     end
   end
 end
