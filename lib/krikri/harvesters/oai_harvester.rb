c7ad8e56083af6505eea9bde99c261b463d4c873
Tom Johnson <thomas.johnson@oregonstate.edu>
2015-01-15 16:28:22 -0800

    Refactor of Krikri::Harvester to accept options

--- lib/krikri/harvesters/oai_harvester.rb
+++ lib/krikri/harvesters/oai_harvester.rb
@@ -3,7 +3,7 @@ module Krikri::Harvesters
   # A harvester implementation for OAI-PMH
   class OAIHarvester
     include Krikri::Harvester
-    attr_accessor :client, :name
+    attr_accessor :client
 
     ##
     # @param opts [Hash] options to pass through to client requests.
@@ -12,8 +12,7 @@ module Krikri::Harvesters
     # @see OAI::Client
     # @see #expected_opts
     def initialize(opts = {})
-      uri = opts.delete(:uri)
-      @name = opts.fetch(:name, nil)
+      super
       @opts = opts.fetch(:oai, {})
       @client = OAI::Client.new(uri)
     end
@@ -48,9 +47,8 @@ module Krikri::Harvesters
     def records(opts = {})
       opts = opts.merge(@opts)
       client.list_records(opts).full.lazy.flat_map do |rec|
-        Krikri::OriginalRecord
-          .build(Krikri::Md5Minter.create(rec.header.identifier, name),
-                 build_record(rec))
+        @record_class.build(mint_id(rec.header.identifier),
+                            record_xml(rec))
 
       end
     end
@@ -60,9 +58,8 @@ module Krikri::Harvesters
     def get_record(identifier, opts = {})
       opts[:identifier] = identifier
       opts = opts.merge(@opts)
-      Krikri::OriginalRecord
-        .build(Krikri::Md5Minter.create(identifier, name),
-               build_record(client.get_record(opts).record))
+      @record_class.build(mint_id(identifier),
+                          record_xml(client.get_record(opts).record))
     end
 
     ##
@@ -79,7 +76,7 @@ module Krikri::Harvesters
 
     private
 
-    def build_record(rec)
+    def record_xml(rec)
       doc = Nokogiri::XML::Builder.new do |xml|
         xml.record('xmlns' => 'http://www.openarchives.org/OAI/2.0/') {
           xml.header {
