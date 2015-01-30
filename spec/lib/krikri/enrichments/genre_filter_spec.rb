21dd6285e77875a168fd836a3282199c4c4f5af9
Tom Johnson <thomas.johnson@oregonstate.edu>
2015-01-29 16:58:32 -0800

    Add a genre filter enrichment
    Removes values that do not match a given list of genre terms.

---
+++ spec/lib/krikri/enrichments/genre_filter_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Krikri::Enrichments::GenreFilter do
+  it_behaves_like 'a field enrichment'
+
+  values = [{ :string => 'removes non-genre terms',
+              :start => 'Not A Term',
+              :end => nil
+            },
+            { :string => 'retains genre terms',
+              :start => 'Book',
+              :end => 'Book'
+            },
+            { :string => 'normalizes string form',
+              :start => "\n bO  O-k!   \t",
+              :end => 'Book'
+            }]
+
+  it_behaves_like 'a string enrichment', values
+end
