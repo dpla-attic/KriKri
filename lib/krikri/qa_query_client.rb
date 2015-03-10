92df4c852e8d7ef834fdd5442b6e35ce560f5c8e
Mark A. Matienzo <mark@dp.la>
2015-03-10 12:46:38 -0400

    More SPARQL queries for QA - missing tests [WIP] [ci skip]

--- lib/krikri/qa_query_client.rb
+++ lib/krikri/qa_query_client.rb
@@ -6,13 +6,61 @@ module Krikri
 
     module_function
 
+    # need methods to handle more complex situations:
+    # * things with prefLabel or providedLabel
+    # * dates - this should probably be its own
+    def values_for_simple_aggregation_predicate(provider_uri, predicate)
+      raise ArgumentError, 'provider_uri must be an RDF::URI' unless
+         provider_uri.respond_to? :to_uri
+      SPARQL_CLIENT.select(:s, :value)
+        .where([:s, RDF::EDM.provider, provider_uri],
+               [:s, RDF::RDFV.type, RDF::ORE.Aggregation])
+        .optional([:s, predicate, :value])
+        .order_by("str(?s) ASC")
+    end
+
+    def values_for_simple_sourceresource_predicate(provider_uri, predicate)
+      raise ArgumentError, 'provider_uri must be an RDF::URI' unless
+         provider_uri.respond_to? :to_uri
+      SPARQL_CLIENT.select(:s, :value)
+        .where([:s, RDF::EDM.provider, provider_uri],
+               [:s, RDF::RDFV.type, RDF::ORE.Aggregation])
+        .optional([:s, RDF::EDM.aggregatedCHO, :sr],
+                  [:sr, predicate, :value])
+        .order_by("str(?s) ASC")
+    end
+
+    def count_for_aggregation_predicate(provider_uri, predicate)
+      raise ArgumentError, 'provider_uri must be an RDF::URI' unless
+         provider_uri.respond_to? :to_uri
+      SPARQL_CLIENT.select(:value, count: { '*' => :count })
+        .where([:s, RDF::EDM.provider, provider_uri],
+               [:s, RDF::RDFV.type, RDF::ORE.Aggregation])
+        .optional([:s, predicate, :value])
+        .group_by(:value)
+        .order_by("?count DESC")
+    end
+
+    def count_for_simple_sourceresource_predicate(provider_uri, predicate)
+      raise ArgumentError, 'provider_uri must be an RDF::URI' unless
+         provider_uri.respond_to? :to_uri
+      SPARQL_CLIENT.select(:value, count: {'*' => :count} )
+        .where([:s, RDF::EDM.provider, provider_uri],
+               [:s, RDF::RDFV.type, RDF::ORE.Aggregation])
+        .optional([:s, RDF::EDM.aggregatedCHO, :sr],
+                  [:sr, predicate, :value])
+        .group_by(:value)
+        .order_by("?count DESC")
+    end
+
     def values_agg_data_provider(provider_uri)
       raise ArgumentError, 'provider_uri must be an RDF::URI' unless
          provider_uri.respond_to? :to_uri
       SPARQL_CLIENT.select(:s, :value)
         .where([:s, RDF::EDM.provider, provider_uri],
                [:s, RDF::RDFV.type, RDF::ORE.Aggregation])
-        .optional([:s, RDF::EDM.dataProvider, :value])
+        .optional([:s, RDF::EDM.dataProvider, :dp],
+                  [:dp, RDF::DPLA.providedLabel, :value])
         .order_by("str(?s) ASC")
     end
 
@@ -22,7 +70,8 @@ module Krikri
       SPARQL_CLIENT.select(:value, count: { '*' => :count })
         .where([:s, RDF::EDM.provider, provider_uri],
                [:s, RDF::RDFV.type, RDF::ORE.Aggregation])
-        .optional([:s, RDF::EDM.dataProvider, :value])
+        .optional([:s, RDF::EDM.dataProvider, :dp],
+                  [:dp, RDF::DPLA.providedLabel, :value])
         .group_by(:value)
         .order_by("?count DESC")
     end
