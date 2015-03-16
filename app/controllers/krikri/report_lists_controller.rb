716ea6c693deb845003801e1aed93e89d9e971d7
Audrey Altman <audrey@dp.la>
2015-03-16 12:43:09 -0400

    add reports lists show route

--- app/controllers/krikri/report_lists_controller.rb
+++ app/controllers/krikri/report_lists_controller.rb
@@ -1,12 +1,15 @@
 module Krikri
   class ReportListsController < ApplicationController
     include Krikri::QaProviderFilter
-    before_action :authenticate_session_provider
     layout 'krikri/application'
 
     def index
+      @validation_report_list = Krikri::ValidationReportList.new.report_list
+    end
+
+    def show
       @validation_report_list = Krikri::ValidationReportList.new
-        .report_list_by_provider(session[:provider])
+          .report_list_by_provider(params[:id])
     end
   end
 end
