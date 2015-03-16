f83badfd85ff2e6e92bda04ac67caa39761ca911
Audrey Altman <audrey@dp.la>
2015-03-16 11:52:08 -0400

    provider by id rather than name

--- app/models/krikri/provider.rb
+++ app/models/krikri/provider.rb
@@ -6,10 +6,18 @@ module Krikri
     # @return Array of Blacklight::SolrResponse::Facets::FacetItem's
     def all
       index_querier = Krikri::IndexQuerier.new
-      default_params = { :rows => 10,
+      default_params = { :rows => 0,
                           :id => '*:*',
-                          'facet.field' => 'provider_name' }
+                          'facet.field' => 'provider_id' }
       index_querier.search(default_params).facets.first.items
+    end
+
+    ##
+    # Find a provider from the search index.
+    # @param String - id of a provider
+    # @return Krikri::SearchIndexDocument
+    def find(id)
+
     end
 
   end
