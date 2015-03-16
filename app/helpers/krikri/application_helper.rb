425d6fb267d53068601352cf050f51cd4919243b
Audrey Altman <audrey@dp.la>
2015-03-16 17:45:54 -0400

    current_provider_name in application helper

--- app/helpers/krikri/application_helper.rb
+++ app/helpers/krikri/application_helper.rb
@@ -1,4 +1,16 @@
 module Krikri
   module ApplicationHelper
+
+    def current_provider_id
+      return params[:provider_id] if params[:provider_id].present?
+      return session[:provider_id] if session[:provider_id].present?
+      return "All providers"
+    end
+
+    # TODO: Make this the name of the provider instead of the id 
+    def current_provider_name
+      current_provider_id
+    end
+
   end
 end
