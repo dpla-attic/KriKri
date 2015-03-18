229d0c6f340c421de87e2d6cee21bbdbe30049f1
Tom Johnson <tom@dp.la>
2015-03-18 16:28:48 -0700

    Add enrichment to strip leading colons/semicolons

---
+++ lib/krikri/enrichments/strip_leading_colons.rb
@@ -0,0 +1,15 @@
+module Krikri::Enrichments
+  ##
+  # Strip leading colons
+  #
+  #   StripLeadingColons.new.enrich_value(";:\tmoominpa()pa;;;")
+  #   # => "\tmoominpa()pa;;;"
+  class StripLeadingColons
+    include Krikri::FieldEnrichment
+
+    def enrich_value(value)
+      return value unless value.is_a? String
+      value.gsub(/^[\;\:]*/, '')
+    end
+  end
+end
