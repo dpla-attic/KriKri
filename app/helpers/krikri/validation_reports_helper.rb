534c2e425c18f16720388d856a90f35568b8fdd1
Audrey Altman <audrey@dp.la>
2015-03-19 15:57:29 -0400

    reports view uses its own helper instead of ValidationReportsList

--- app/helpers/krikri/validation_reports_helper.rb
+++ app/helpers/krikri/validation_reports_helper.rb
@@ -1,5 +1,17 @@
 module Krikri
   module ValidationReportsHelper
-    include Krikri::SearchResultsHelperBehavior
+    
+    def render_validation_report(report)
+      return validation_report_title(report) if report.items.first.hits == 0
+      link_to validation_report_title(report), { :controller => 
+        'validation_reports', :action => 'show', :id => report.name,
+        :provider_id => params[:provider_id] }, :method => :get
+    end
+
+    def validation_report_title(report)
+      return "#{report.name} missing (#{report.items.first.hits} record)" if
+        report.items.first.hits == 1
+      "#{report.name} missing (#{report.items.first.hits} records)"
+    end
   end
 end
