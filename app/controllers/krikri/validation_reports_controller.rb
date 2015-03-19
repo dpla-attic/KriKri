34eecc7a063f0cfa5be3ea338a3775b8b12e5e64
Audrey Altman <audrey@dp.la>
2015-03-19 18:21:19 -0400

    basic validation report show controller and view

--- app/controllers/krikri/validation_reports_controller.rb
+++ app/controllers/krikri/validation_reports_controller.rb
@@ -1,52 +1,18 @@
 module Krikri
-  ##
-  # Marshals SolrDocuments for views.
-  # Sets default Solr request params for Validation Reports.
-  #
-  # ValidationReportsController inherits from the host application's
-  # ApplicationController.  It does not interit from Krikri's
-  # ApplicationController.
   class ValidationReportsController < CatalogController
     include Krikri::QaProviderFilter
     before_action :authenticate_user!, :session_provider
-    before_action :valid_params, :only => :index
-    self.solr_search_params_logic += [:records_by_provider]
-
-    ##
-    # ValidationReportsController has access to views in the following
-    # directories:
-    #   krikri/validation_reports
-    #   catalog (defined in Blacklight)
-    # It inherits view templates from the host application's
-    # ApplicationController.  It uses krikri's application layout:
     layout 'krikri/application'
 
-    configure_blacklight do |config|
-
-      # Default parameters to send to solr for all search-like requests.
-      config.default_solr_params = {
-        :qt => 'standard',
-        :rows => 100
-      }
-
-      # solr fields to be displayed in the index (search results) view
-      #   The ordering of the field names is the order of the display
-      config.add_index_field 'sourceResource_title', :label => 'Title',
-                             helper_method: 'link_to_show'
-      config.add_index_field 'id', :label => 'ID', helper_method: 'link_to_show'
-      config.add_index_field 'isShownAt_id', :label => 'Is Shown At',
-                             helper_method: 'make_this_a_link'
-
-      config.show.route = { controller: 'records' }
-
-      config.solr_document_model = Krikri::SearchIndexDocument
+    def show
+      provider_id = params[:provider_id]
+      @response = ValidationReport.new.find(params[:id]) do
+        self.provider_id = provider_id
+      end
+      @documents = @response.documents
     end
 
     private
-
-    def valid_params
-      redirect_to :reports if !params['report_name'] || !params['q']
-    end
 
     def session_provider
       session[:provider_id] = params[:provider_id].present? ? 
