5abb5fc95416f1f14d66ce2a6758b5118e2b985d
Audrey Altman <audrey@dp.la>
2015-03-19 23:06:18 -0400

    delete unused helper methods

--- lib/krikri/search_results_helper_behavior.rb
+++ lib/krikri/search_results_helper_behavior.rb
@@ -13,30 +13,6 @@ module Krikri
       link_to field_or_opts, url_for_document(document)
     end
 
-    # This method is used to make display fields into hyperlinks.
-    def make_this_a_link(options = {})
-      # options[:document] # the original document
-      # options[:field] # the field to render
-      # options[:value] # the value of the field
-
-      link_to options[:value], options[:value]
-    end
-
-    # This method is used to link display fields to the document's show path.
-    def link_to_show(options = {})
-      # options[:document] # the original document
-      # options[:field] # the field to render
-      # options[:value] # the value of the field
-
-      link_to_document options[:document], options[:value]
-    end
-
-    def render_document_field_data(document, key)
-      if document[key]
-        render_index_field_value document, :field => key
-      end
-    end
-
     # Disable bookmarks.
     def render_bookmarks_control?
       false
