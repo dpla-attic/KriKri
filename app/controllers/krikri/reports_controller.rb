e5d4f8de49f034c3c2920b0f51c7b3bc553166c2
Audrey Altman <audrey@dp.la>
2015-03-20 14:51:05 -0400

    ApplicationController and ReportsController @current_provider instead of session[:provider_id]

--- app/controllers/krikri/reports_controller.rb
+++ app/controllers/krikri/reports_controller.rb
@@ -1,20 +1,12 @@
 module Krikri
   class ReportsController < ApplicationController
     layout 'krikri/application'
-    before_action :session_provider
 
     def index
-      provider_id = params[:provider_id]
+      provider_id = @current_provider
       @validation_reports = Krikri::ValidationReport.new.all do 
         self.provider_id = provider_id
       end
-    end
-
-    private
-
-    def session_provider
-      session[:provider_id] = params[:provider_id].present? ? 
-        params[:provider_id] : nil
     end
   end
 end
