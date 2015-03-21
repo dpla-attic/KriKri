16ff5dcefe4370622a007385fea9822d879a2815
Audrey Altman <audrey@dp.la>
2015-03-21 00:42:41 -0400

    improve views and ApplicationHelper - remove session variable dependencies

--- app/helpers/krikri/application_helper.rb
+++ app/helpers/krikri/application_helper.rb
@@ -1,38 +1,21 @@
 module Krikri
   module ApplicationHelper
 
-    def current_provider_id
-      return session[:provider_id].present? ? session[:provider_id] : nil
-    end
-
-    # TODO: Make this the name of the provider instead of the id 
-    def current_provider_name
-      return "All Providers" unless current_provider_id.present?
-      Krikri::Providers.new.find(current_provider_id)['provider_name']
-    end
-
+    ##
+    # @param provider [String, nil]
+    # @return [String]
     def provider_name(provider)
       return "All Providers" unless provider.present?
-      provider
+      Krikri::Provider.new.find(provider)['provider_name']
     end
 
-    ##### INSTEAD javascript function when dropdown button is clicked:
-    # If session-sensitve controller, change params and re-render page
-    # If not, change session state and re-render page. 
-    # Make other links in menu response to session state.
-    ##
-    # Returns all providers that are not currently set as session provider
     # @return Array of Blacklight::SolrResponse::Facets::FacetItem's
-    # 
-    # TODO return names of providers along with ids
     def available_providers
-      Krikri::Provider.new.all.delete_if do |p| 
-        local_name(p.value) == current_provider_id
-      end
+      Krikri::Provider.new.all
     end
 
     ##
-    # @param [String]
+    # @param uri [String]
     # @return [String]
     # Sample use:
     #   local_name('http/my_domain/0123') => '0123'
@@ -40,31 +23,25 @@ module Krikri
       uri.split('/').last.html_safe
     end
 
+    ##
+    # Link to the current page, changing the provider param or id to the given
+    # value.
+    # @param provider [String, nil]
     def link_to_current_page_by_provider(provider)
-      if params[:controller] == 'krikri/providers'
-        return link_to_provider_page(provider)
-      end
-
-      if provider == :all
-        params.delete :provider_id
-        provider_name = "All Providers"
-      else
-        params[:provider_id] = provider
-        provider_name = provider
-      end
-
-      link_to provider_name, params
+      return link_to_provider_page(provider) if params[:controller] == 
+        'krikri/providers'
+      params.delete :provider if provider == nil
+      params[:provider] = provider if provider.present?
+      link_to provider_name(provider), params
     end
 
     ##
     # Link to the ProviderController's :index or :show route, using the given
     # provider id in the case of :show.
-    # @param provider [String] a provider id
-    #   Sample use: link_to_provider_page(:all)
-    #   Sample use: link_to_provider_page('0123')
+    # @param provider [String, nil]
     def link_to_provider_page(provider)
-      return link_to "All Providers", providers_path if provider == :all
-      return link_to provider, provider_path(provider)
+      return link_to provider_name(provider), providers_path if provider == nil
+      return link_to provider_name(provider), provider_path(provider)
     end
 
   end
