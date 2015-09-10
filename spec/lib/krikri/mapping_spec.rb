b0c6a1a00148aea05dc8f605ff0d703bb2568f47
Tom Johnson <tom@dp.la>
2015-09-10 12:00:48 -0700

    Error handling in Mapping
    Introduces a `Mapping::Error` that tracks multiple errors for a given
    record and handles messaging.
    
    In the `Mapping`, we catch errors for each declaration on a given
    record, storing them with the relevant property name, and re-raise. The
    `Mapper` then simply catches the error and logs its message.
    
    This simplifies handling outside the DSL code, and creates richer error
    messages.

--- spec/lib/krikri/mapping_spec.rb
+++ spec/lib/krikri/mapping_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Krikri::Mapping do
 
-  let(:target_class) { double }
-  let(:parser) { double }
+  let(:target_class) { double('target class') }
+  let(:parser) { double('parser') }
   let(:parser_args) { [1,2,3] }
 
   describe '#new' do
@@ -28,15 +28,52 @@ describe Krikri::Mapping do
       expect(new_mapping.process_record(record)).to be_a klass
     end
 
-    context 'with parser' do
+    shared_context 'property declarations' do
       before do
-        target_instance = double
         allow(target_class).to receive(:new).and_return(target_instance)
         allow(target_instance).to receive(:my_property=).and_return('')
+        allow(parser).to receive(:parse).and_return(record)
         subject.my_property ''
       end
 
       subject { described_class.new(target_class, parser, *parser_args) }
+      let(:target_instance) { double('target instance') }
+    end
+    
+    describe 'error handling' do  
+      include_context 'property declarations'
+
+      before do
+        allow(target_instance).to receive(:error_property=).and_raise error
+        subject.error_property 'moomin'
+      end
+
+      let(:error) { RuntimeError.new }
+    
+      it 'catches errors and raises Mapping::Error ' do
+        expect { subject.process_record(record) }
+          .to raise_error described_class::Error
+      end
+
+      it 'tracks failed properties' do
+        begin 
+          subject.process_record(record)
+        rescue described_class::Error => err
+          expect(err.properties).to contain_exactly :error_property
+        end
+      end
+
+      it 'stores properties and errors in Mapping::Error' do
+        begin 
+          subject.process_record(record)
+        rescue described_class::Error => err
+          expect(err.errors[:error_property]).to eq error
+        end
+      end
+    end
+
+    context 'with parser' do
+      include_context 'property declarations'
 
       it 'parses record before mapping' do
         expect(parser)
