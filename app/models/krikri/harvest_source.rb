edaf660c5acb4807a8ddaf30ec6976b8a870420f
Tom Johnson <tom@dp.la>
2015-12-10 11:48:10 -0800

    [WIP] Build out HarvestSource
    Adds validation for `#source_type` and the ability to build an
    appropriate harvester. `#build_harvester` has no opts support at this
    time.

--- app/models/krikri/harvest_source.rb
+++ app/models/krikri/harvest_source.rb
@@ -4,8 +4,50 @@ module Krikri
   class HarvestSource < ActiveRecord::Base
     belongs_to :institution
     validates_presence_of :institution, :name, :source_type, :uri
+
+    validates_each :source_type do |record, attr, val|
+      if val.nil? || 
+         !(Krikri::Harvester::Registry.registered?(val.downcase.to_sym))
+        record.errors.add(:attr, 'source_type must exist in Harvester registry')
+      end
+    end
+
     validates :uri,
               format: { with: URI.regexp },
               if: proc { |a| a.uri.present? }
+    
+    def build_harvester
+      harvester_class.new(build_opts)
+    end
+
+    # def validate_options!
+    #   harvester_class.validate_opts!(opts)
+    # end
+
+    private
+
+    ##
+    # @private
+    # @retern [Hash] harvester options
+    def build_opts
+      opts = { uri: uri }
+      # opts[harvester_class.expected_opts[:key]] = options
+      # harvester_class.validate_opts!(opts)
+      opts
+    end
+    
+    ##
+    # @private
+    # @return [Class] A class implementing `Krikri::Harvester`
+    def harvester_class
+      Krikri::Harvester::Registry.get(source_key)
+    end
+
+    ##
+    # @private
+    # @return [Symbol] the Harvester::Registry key for the harvester
+    def source_key
+      source_type.downcase.to_sym
+    end
   end
 end
--- spec/models/harvest_source_spec.rb
+++ spec/models/harvest_source_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Krikri::HarvestSource, type: :model do
+  subject { build(:krikri_harvest_source) }
 
   it 'is a HarvestSource' do
     expect(subject).to be_a Krikri::HarvestSource
@@ -8,7 +9,7 @@ describe Krikri::HarvestSource, type: :model do
 
   describe 'validation' do
     it 'has a valid factory' do
-      expect(build(:krikri_harvest_source)).to be_valid
+      expect(subject).to be_valid
     end
 
     it 'validates institution presence' do
@@ -23,12 +24,26 @@ describe Krikri::HarvestSource, type: :model do
       expect(build(:krikri_harvest_source, source_type: nil)).not_to be_valid
     end
 
+    it 'validates existence of source_type ' do
+      expect(build(:krikri_harvest_source, source_type: 'FAKE')).not_to be_valid
+    end
+
     it 'validates URI presence' do
       expect(build(:krikri_harvest_source, uri: nil)).not_to be_valid
     end
 
     it 'validates URI format' do
       expect(build(:krikri_harvest_source, uri: 'bogus')).not_to be_valid
+    end
+  end
+
+  describe '#build_harvester' do
+    it 'returns a harvester instance' do
+      expect(subject.build_harvester).to be_a Krikri::Harvester
+    end
+
+    it 'returns harvester class for given source_type' do
+      expect(subject.build_harvester).to be_a Krikri::Harvesters::OAIHarvester
     end
   end
 end
