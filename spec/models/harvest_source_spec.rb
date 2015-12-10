48bb03470d160806a8e0a965a2df67256794d2d8
Tom Johnson <tom@dp.la>
2015-12-10 10:09:03 -0800

    Add HarvestSource validation specs; use factories

--- spec/models/harvest_source_spec.rb
+++ spec/models/harvest_source_spec.rb
@@ -2,22 +2,33 @@ require 'spec_helper'
 
 describe Krikri::HarvestSource, type: :model do
 
-  let(:institution) { Krikri::Institution.create!(name: 'Test') }
-
   it 'is a HarvestSource' do
     expect(subject).to be_a Krikri::HarvestSource
   end
 
-  it 'requires URIs to be well-formed' do
-    baddata = {
-      institution: institution,
-      name: 'Test Source',
-      source_type: 'OAI',
-      metadata_schema: 'MODS',
-      uri: 'bogus'
-    }
-    expect { Krikri::HarvestSource.create!(baddata) }
-      .to raise_error(ActiveRecord::RecordInvalid)
-  end
+  describe 'validation' do
+    it 'has a valid factory' do
+      expect(build(:krikri_harvest_source)).to be_valid
+    end
 
+    it 'validates institution presence' do
+      expect(build(:krikri_harvest_source, institution: nil)).not_to be_valid
+    end
+
+    it 'validates name presence' do
+      expect(build(:krikri_harvest_source, name: nil)).not_to be_valid
+    end
+
+    it 'validates source_type presence' do
+      expect(build(:krikri_harvest_source, source_type: nil)).not_to be_valid
+    end
+
+    it 'validates URI presence' do
+      expect(build(:krikri_harvest_source, uri: nil)).not_to be_valid
+    end
+
+    it 'validates URI format' do
+      expect(build(:krikri_harvest_source, uri: 'bogus')).not_to be_valid
+    end
+  end
 end
