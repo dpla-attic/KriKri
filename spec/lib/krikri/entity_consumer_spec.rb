85ca712a669c8ce24ab0dc1700cb566231501497
Tom Johnson <tom@dp.la>
2016-02-23 15:33:08 -0800

    Encapsulate URI lookup logic in Activity
    Moves activity URI lookup to `Activity` & exposes more public interfaces
    for working with activity URIs.
    
    Refactors `EntityConsumer` to rely on this logic, instead of using a
    coarse string based matcher.
    
    `EntityConsumer` no longer matches uris that end with the same ID,
    despite not being the correct URI. For example, `blahblahblah2`, would
    have matched the `Activity` with id == 2, previously.

--- spec/lib/krikri/entity_consumer_spec.rb
+++ spec/lib/krikri/entity_consumer_spec.rb
@@ -9,10 +9,9 @@ describe Krikri::EntityConsumer do
   end
 
   context 'as a mapper agent' do
-    let(:generator_uri) { 'http://example.org/ldp/activity/2' }
-    let(:opts) do
-      {name: :agent_map, generator_uri: generator_uri}
-    end
+    let(:generator_uri) { Krikri::Activity.base_uri / 2 }
+    let(:opts) { { name: :agent_map, generator_uri: generator_uri } }
+
     let(:mapper_agent) { Krikri::Mapper::Agent.new(opts) }
 
     describe '#assign_generator_activity!' do
@@ -21,7 +20,5 @@ describe Krikri::EntityConsumer do
           .to respond_to(:entities)
       end
     end
-
   end
-
 end
