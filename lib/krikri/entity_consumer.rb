f0e4052cbedd5426215f8697d45f9b412e509824
Tom Johnson <tom@dp.la>
2016-02-24 16:09:46 -0800

    Refactor EntityConsumer for generalized sources
    `EntityConsumer` now responds to `#entities` directly, allowing use of
    various entity sources: including, but not limited to, Activities.

--- lib/krikri/entity_consumer.rb
+++ lib/krikri/entity_consumer.rb
@@ -1,15 +1,21 @@
 module Krikri
   ##
-  # A mixin for classes, like certain `SoftwareAgent`s, that generates entities.
-  # For example, a mapper usually generates `DPLA::MAP::Aggregation`s, such that
-  # Mapper::Agent includes EntityConsumer, and a mapper agent is instantiated
-  # with
-  #   a = Krikri::Mapper::Agent.new({
-  #     generator_uri: 'http://some.org/activity/1'
-  #   })
-  # such that
-  #   a.generator_activity
-  # returns a Krikri::Activity
+  # A mixin for `Krikri::SoftwareAgent`s that use entities. Provides a 
+  # mechanism for setting an `#entity_source` and consuming entities.
+  #
+  # For backwards compatability, this supports an older interface where entities
+  # are selected based on a `generator_activity`.
+  # 
+  # @example the deprecated interface
+  #   class AnAgent
+  #     include Krikri::EntityConsumer
+  #   end
+  #   
+  #   agent = AnAgent.new
+  #   agent.assign_generator_activity!(generator_uri: 
+  #     Krikri::Activity.find(1).rdf_subject)
+  #
+  #   agent.generator_activity.entities
   #
   module EntityConsumer
     extend ActiveSupport::Concern
@@ -29,14 +35,37 @@ module Krikri
     # because not every activity will modify its entities with provenance
     # messages; an indexing activity, in particular.  In this case an LDP URI
     # representing the activity is not relevant.
-    #
+    # 
     # @see Krikri::Mapper::Agent
     # @see Krikri::Harvester
     def assign_generator_activity!(opts)
       if opts.include?(:generator_uri)
         generator_uri = opts.delete(:generator_uri)
-        @generator_activity = Krikri::Activity.from_uri(generator_uri)
+        @entity_source = 
+          @generator_activity = Krikri::Activity.from_uri(generator_uri)
       end
+    end
+
+    ##
+    # @return [Enumerator<Krikri::LDP::Resource>] entities this agent will use
+    def entities
+      entity_source ? entity_source.entities : []
+    end
+
+    ##
+    # @return [#entities, nil]
+    def entity_source
+      @entity_source
+    end
+
+    ##
+    # Sets the entity source to a new instance of the provided class, 
+    # initialized with the provided arguments and block.
+    #
+    # @param klass [Class] a class with an instance method `#entities`
+    # @return [void]
+    def set_entity_source!(klass, *args, &block)
+      @entity_source = klass.new(*args, &block)
     end
   end
 end
