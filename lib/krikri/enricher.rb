d11f6259b5a76ca7546eb09733e0515703db4736
Tom Johnson <tom@dp.la>
2016-02-26 11:41:16 -0800

    Refactor SoftwareAgent's use of entity_behavior
    Use a default entity_behavior at the class level. Set @entity_behavior
    on intialization to avoid ||= in the accessor method.

--- lib/krikri/enricher.rb
+++ lib/krikri/enricher.rb
@@ -34,14 +34,6 @@ module Krikri
     end
 
     ##
-    # @see Krikri::Activity#entities
-    # @see Krikri::EntityBehavior
-    # @see Krikri::SoftwareAgent#entity_behavior
-    def entity_behavior
-      @entity_behavior ||= Krikri::AggregationEntityBehavior
-    end
-
-    ##
     # Create a new Enricher, given a hash of options:
     #   generator_uri:  the LDP URI of the Activity that generated the mapped
     #      records that this one will enrich.
@@ -53,6 +45,7 @@ module Krikri
     def initialize(opts = {})
       @generator_uri = RDF::URI(opts.fetch(:generator_uri))
       @chain = deep_sym(opts.fetch(:chain) { {} })
+      @entity_behavior = self.class.entity_behavior
       assign_generator_activity!(opts)
     end
 
