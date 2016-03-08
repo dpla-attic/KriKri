a6264fa5169f1047aa36688a33a469c1e402745d
Tom Johnson <tom@dp.la>
2016-03-08 10:46:40 -0800

    Remove misleading docs from ProvenanceQueryClient
    The use of `query#filter` in `Krikri::ProvenanceQueryClient` is as
    intended by the `SPARQL::Client` library. There are no plans to develop
    a DSL for constructing filters in general; so using the string arguments
    isn't really a hack.
    
    Slight doc fixups and formatting are included.

--- lib/krikri/provenance_query_client.rb
+++ lib/krikri/provenance_query_client.rb
@@ -11,15 +11,14 @@ module Krikri
     # given.
     #
     # @param activity_uri [#to_uri] the URI of the activity to search
-    #
     # @param include_invalidated [Boolean]  Whether to include entities that
     #   have been invalidated with <http://www.w3.org/ns/prov#invalidatedAtTime>
     #
-    # @see https://www.w3.org/TR/prov-o/#invalidatedAtTime
-    # @see Krikri::LDP::Invalidatable
-    #
     # @return [RDF::SPARQL::Query] a query object that, when executed, will
     #   give solutions containing the URIs for the resources in `#record`.
+    #
+    # @see https://www.w3.org/TR/prov-o/#invalidatedAtTime
+    # @see Krikri::LDP::Invalidatable
     def find_by_activity(activity_uri, include_invalidated = false)
       raise ArgumentError, 'activity_uri must be an RDF::URI' unless
         activity_uri.respond_to? :to_term
@@ -28,30 +27,22 @@ module Krikri
                 [RDF::PROV.wasGeneratedBy, '|', RDF::DPLA.wasRevisedBy],
                 activity_uri.to_term])
 
-      if include_invalidated
-        query
-      else
-        # We need to say "and if RDF::PROV.invalidatedAtTime is not set."
-        #
-        # The SPARQL query should be:
-        #
-        # PREFIX prov: <http://www.w3.org/ns/prov#>
-        # SELECT * WHERE {
-        #   ?subject prov:wasGeneratedBy  <http://xampl.org/ldp/activity/n> .
-        #   FILTER NOT EXISTS { ?subject prov:invalidatedAtTime ?x }
-        # }
-        #
-        # ... However there doesn't appear to be a way of constructing
-        # 'FILTER NOT EXISTS' with SPARQL::Client.  Instead, we've managed to
-        # hack the following solution together.
-        #
-        # SPARQL::Client#filter is labeled @private in its YARD comment (and
-        # has no other documentation) but it's not private, at least for
-        # now.
-        query.filter \
-          'NOT EXISTS ' \
-          '{ ?record <http://www.w3.org/ns/prov#invalidatedAtTime> ?x }'
-      end
+      return query if include_invalidated
+
+      # When `include_invalidated` is false, we need to say "and if 
+      # RDF::PROV.invalidatedAtTime is not set."
+      #
+      # The SPARQL query should be:
+      #
+      # ```
+      #   PREFIX prov: <http://www.w3.org/ns/prov#>
+      #   SELECT * WHERE {
+      #     ?subject prov:wasGeneratedBy  <http://xampl.org/ldp/activity/n> .
+      #     FILTER NOT EXISTS { ?subject prov:invalidatedAtTime ?x }
+      #   }
+      # ```
+      query.filter 'NOT EXISTS ' \
+                   "{ ?record #{RDF::PROV.invalidatedAtTime.to_base} ?x }"
     end
   end
 end
