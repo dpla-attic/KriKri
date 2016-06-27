06874277edf0be86f9ae98b3465bf000b10fe154
Tom Johnson <tom@dp.la>
2016-06-27 15:48:48 -0700

    Refactor & Document MappingDSL #to_procs
    MappingDSL Declarations were somewhat messy and under-documented,
    particularly the `#to_proc` methods. This refactors slightly, removing
    some undue complexity. It also documents the Declaration classes much
    more thoroughly.

--- lib/krikri/mapping_dsl/property_declaration.rb
+++ lib/krikri/mapping_dsl/property_declaration.rb
@@ -3,15 +3,52 @@ module Krikri::MappingDSL
   # Specifies a mapping between a property name and its mapped value(s).
   # Deals with static properties (given a specific value or values), and
   # dynamic properties (where values are modified by a block).
+  #
+  # @example a basic declaration
+  #   class Book; attr_accessor :author; end
+  #
+  #   dec = Krikri::MappingDSL::PropertyDeclaration.new(:author, 
+  #     ['Moomin', 'Snuffkin'])
+  #
+  #   book = Book.new
+  #   dec.to_proc.call(book, nil) # nil stands in for a record.
+  #   
+  #   book.author # => ['Moomin', 'Snuffkin']
+  #
+  #
+  # @example a declaration with a callable value
+  #   class Book; attr_accessor :author; end
+  #
+  #   values = lambda { |_| ['Moomin', 'Snuffkin'] }
+  #   dec = Krikri::MappingDSL::PropertyDeclaration.new(:author, values)
+  #
+  #   book = Book.new
+  #   dec.to_proc.call(book, nil) # nil stands in for a record.
+  #   
+  #   book.author # => ['Moomin', 'Snuffkin']
+  #   
   class PropertyDeclaration
     attr_reader :name, :value
 
+    ##
+    # Initializes a declaration with a given name and value. `value` may 
+    # respond to `#call`, which will be called with a record to generate the
+    # values.
+    #
+    # @param name  [Symbol]
+    # @param value [#call, Object] 
+    # @param _opts [Hash] A hash of options for for the declaration. default: {}
+    #
+    # @raise ArgumentError  when a block with arity other than 1 is passed
     def initialize(name, value, _opts = {}, &block)
       if block_given?
-        raise 'Block must have arity of 1 to be applied to property' unless
-          block.arity == 1
+        unless block.arity == 1
+          raise(ArgumentError, 
+                'Block must have arity of 1 to be applied to property')
+        end
         @block = block
       end
+
       @name = name
       @value = value
     end
@@ -24,13 +61,15 @@ module Krikri::MappingDSL
     # OriginalRecord as an argument to determine the value.
     #
     # @return [Proc] a proc that can be used to generate a value for the named
-    # property.
+    #   property.
     def to_proc
-      block = @block if @block
+      block = @block
       value = @value
+      
       lambda do |target, record|
         value = value.call(record) if value.respond_to? :call
         return target.send(setter, value) unless block
+
         if value.is_a? Enumerable
           values = value.map { |v| instance_exec(v, &block) }
           target.send(setter, values)
@@ -42,6 +81,8 @@ module Krikri::MappingDSL
 
     private
 
+    ##
+    # @return [Symbol] A symbol for the setter method; e.g. `:name=`
     def setter
       "#{@name}=".to_sym
     end
